0d43740c28f4db53be6e9c95cffb0a43c169a4d1
Erickfer1726 <erickfer1703@gmail.com>
2021-10-04 10:21:44 -0500

    actualizando

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,7 @@
 import React from "react";
-import "./Checkout.css";
-import Subtotal from "./Subtotal";
-import { useStateValue } from "./StateProvider";
-import CheckoutProduct from "./CheckoutProduct";
+import "Checkout.css";
+import { useStateValue } from "StateProvider";
+import CheckoutProduct from "CheckoutProduct";
 
 function Checkout() {
   const [{ basket, user }, dispatch] = useStateValue();
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,23 @@
-import firebase from "firebase";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDTtgkvV1cQeP8FcdFnIVhybn_ESPTLgI8",
-    authDomain: "sprint-3-1b300.firebaseapp.com",
-    projectId: "sprint-3-1b300",
-    storageBucket: "sprint-3-1b300.appspot.com",
-    messagingSenderId: "265697427686",
-    appId: "1:265697427686:web:7ede2d90a4a6c2e8007b27",
-    measurementId: "G-R7NDW7K7W9"
+  apiKey: "AIzaSyDTtgkvV1cQeP8FcdFnIVhybn_ESPTLgI8",
+  authDomain: "sprint-3-1b300.firebaseapp.com",
+  projectId: "sprint-3-1b300",
+  storageBucket: "sprint-3-1b300.appspot.com",
+  messagingSenderId: "265697427686",
+  appId: "1:265697427686:web:7ede2d90a4a6c2e8007b27",
+  measurementId: "G-R7NDW7K7W9",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-
-export { db, auth };
+// Initialize Firebase
+const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
